928eeec9d8c6c002f0ae20e13fe6687357ec2b2d
JohnFam123 <minh20033theboy@gmail.com>
2024-10-06 01:40:23 +0700

    add loop and sleep

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 from models import Recipe
 from extract import group_by_pairs, extract_recipe
+from time import sleep
+import pandas as pd
+import threading
 
 def extract_recipes(html: str):
-    soup = BeautifulSoup(html, features="html.parser")
-    ingredient_page_content = soup.find("div", class_="ingredient-page-content")
-    tbody = ingredient_page_content.find_all("tbody")[1]
-    recipes = tbody.find_all("tr", recursive=False)
-    recipe_groups = group_by_pairs(recipes)
+    try:
+        soup = BeautifulSoup(html, features="html.parser")
+        ingredient_page_content = soup.find("div", class_="ingredient-page-content")
+        tbody = ingredient_page_content.find_all("tbody")[1]
+        recipes = tbody.find_all("tr", recursive=False)
+        recipe_groups = group_by_pairs(recipes)
+    except:
+        return []
 
     return [extract_recipe(recipe_group) for recipe_group in recipe_groups]
 
@@ -23,67 +29,33 @@ def fetch_page(page=1):
     )
     return extract_recipes(res.text)
 
+def export_to_csv(recipes, type="success"):
+    headers = ("title", "style", "size", "og", "fg", "abv", "ibu", "color", "views", "brewed", "author", "boil_size",
+           "boil_time", "boil_gravity", "efficiency", "mash_thickness", "sugar_scale", "brew_method", "pitch_rate",
+           "primary_temp","priming_method", "primary_method", "priming_amount", "creation_date", "notes")
+    
+    print (len(recipes))
+    df = pd.DataFrame(recipes, columns=headers)
+    if type == "success":
+        print ("Exporting to CSV - Number of recipes:")
+        df.to_csv("recipes.csv", index=False, mode='a')
+    else:
+        print ("Dump csv files:")
+        df.to_csv("recipes_error.csv", index=False)
 
 def main():
-    for page in range(1, 10):
-        page_content = fetch_page(page)
+    page_content = []
+    for page in range(1000, 15000):
+        single_page_content = fetch_page(page)
+        while (single_page_content == []):
+            print (f"Error on page {page}")
+            single_page_content = fetch_page(page)
+            sleep(1.5)
         
-        for recipe in page_content:
-            (
-                title,
-                style,
-                size,
-                og,
-                fg,
-                abv,
-                ibu,
-                color,
-                views,
-                brewed,
-                author,
-                boil_size,
-                boil_time,
-                boil_gravity,
-                efficiency,
-                mash_thickness,
-                sugar_scale,
-                brew_method,
-                pitch_rate,
-                primary_temp,
-                priming_method,
-                priming_method,
-                priming_amount,
-                creation_date,
-                notes,
-            ) = recipe
-
-            print("title: ", title)
-            print("style: ", style)
-            print("size: ", size)
-            print("og: ", og)
-            print("fg: ", fg)
-            print("abv: ", abv)
-            print("ibu: ", ibu)
-            print("color: ", color)
-            print("views: ", views)
-            print("brewed: ", brewed)
-
-            print("author: ", author)
-            print("boil_size: ", boil_size)
-            print("boil_time: ", boil_time)
-            print("boil_gravity: ", boil_gravity)
-            print("efficiency: ", efficiency)
-            print("mash_thickness: ", mash_thickness)
-            print("sugar_scale: ", sugar_scale)
-            print("brew_method: ", brew_method)
-            print("pitch_rate: ", pitch_rate)
-            print("primary_temp: ", primary_temp)
-            print("primary_method: ", priming_method)
-            print("priming_amount: ", priming_amount)
-            print("creation_date: ", creation_date)
-            print("notes: ", notes)
-
-            print(".\n.\n.\n.\n.\n.\n.\n")
+        page_content+= single_page_content
+        sleep(1.5)
+    
+    export_to_csv(page_content)
 
 
 if __name__ == "__main__":
