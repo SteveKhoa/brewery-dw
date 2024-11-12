27fb0639d07959e05882816c61a9ff5ab805e073
Khoa LTN <vietnamkhoale@gmail.com>
2024-11-12 10:04:57 +0700

    feat: add superset.sh as init script

---
+++ docker-entrypoint-initdb.d/superset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+superset fab create-admin --username admin --firstname Handsome --lastname Admin --email admin@admin.com --password admin
+
+superset db upgrade
+
+superset init
+
+# Starting server
+/bin/sh -c /usr/bin/run-server.sh
