bc21c5b052f864c502da8ea553425fde02f3356d
Khoa LTN <vietnamkhoale@gmail.com>
2024-11-13 23:12:51 +0700

    feat: generate fake dim_time

--- dbt_brewery/models/dim_time.sql
+++ dbt_brewery/models/dim_time.sql
@@ -1,5 +1,6 @@
-select 
-    generateUUIDv4() as uuid, 
-    toDayOfWeek(toDate('2024/11/08')) as day,
-    toMonth(toDate('2024/11/08')) as month,
-    toYear(toDate('2024/11/08')) as year
+SELECT
+    generateUUIDv4() as uuid,
+    toDayOfWeek(toDate('2024/11/08') + number) as day,
+    toMonth(toDate('2024/11/08') + number) as month,
+    toYear(toDate('2024/11/08') + number) as year
+FROM numbers(10)
