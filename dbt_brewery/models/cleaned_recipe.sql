8ea85606a73eda375d528b8a796e1efbde928f8f
Khoa LTN <vietnamkhoale@gmail.com>
2024-11-16 21:25:47 +0700

    fix: cleaned_recipe contains less columns

--- dbt_brewery/models/cleaned_recipe.sql
+++ dbt_brewery/models/cleaned_recipe.sql
@@ -9,33 +9,36 @@ color_mapping as (
         CASE -- mapping color units '°L' to floats
             WHEN UPPER(substringIndex(color, ' ', -1)) = '°L' THEN CAST(substringIndex(color, ' ', 1) AS FLOAT)
             ELSE NULL -- Handle unexpected cases or units
-        END AS color_transformed
+        END AS color_transformed,
+        CASE
+            WHEN views LIKE '%K' THEN CAST(REPLACE(views, 'K', '') AS DECIMAL) * 1000
+            ELSE CAST(views as decimal)
+        END as views_transformed,
+        CASE
+            WHEN boil_time = 'N/A' THEN NULL
+            ELSE CAST(boil_time AS decimal)
+        END as boil_time_transformed,
+        CASE
+            WHEN boil_gravity = 'N/A' THEN NULL
+            ELSE CAST(boil_gravity AS decimal)
+        END as boil_gravity_transformed,
+        CASE
+            WHEN efficiency = 'N/A' THEN NULL
+            ELSE CAST(efficiency AS float)
+        END as efficiency_transformed
     from wrong_title_cleaned
 )
 
 select 
     title,
     style,
-    size,
-    og,
-    fg,
-    abv,
-    ibu,
+    cast(abv as float) as abv,
+    cast(ibu as float) as ibu,
     color_transformed as color,
-    views,
-    brewed,
-    author,
-    boil_size,
-    boil_time,
-    boil_gravity,
-    efficiency,
-    mash_thickness,
-    sugar_scale,
-    brew_method,
-    pitch_rate,
-    primary_temp,
-    priming_method,
-    priming_amount,
-    creation_date,
-    notes
+    views_transformed as views,
+    cast(brewed AS FLOAT) as brewed,
+    boil_time_transformed as boil_time,
+    boil_gravity_transformed as boil_gravity,
+    efficiency_transformed as efficiency,
+    parseDateTimeBestEffort(creation_date) as creation_date
 from color_mapping
