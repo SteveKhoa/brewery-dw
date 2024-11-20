a4ecc0dd24eba30f232678a0041837addfd9bb24
Duy Tran Nhat <nhatduy240203@gmail.com>
2024-11-20 15:20:46 +0700

    chore: add wsl version scripts

--- dbtrun.sh
+++ dbtrun.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-export $(grep -v '^#' .env | xargs)
-
-# Change directory to dbt_brewery
+#env $(cat .env | xargs) > /dev/null
 cd dbt_brewery
-
-# Run dbt with specified number of threads
-dbt run --threads 4 "$@"
+dbt run --threads 4 $@
--- wsl_create.sh
+++ wsl_create.sh
@@ -12,8 +12,8 @@ docker run \
     -p 18123:8123 \
     -p 19000:9000 \
     -p 19009:9009 \
-    -v clickhouse-volume:/var/lib/clickhouse/: \
-    -v clickhouse-volume:/var/log/clickhouse-server/:\
-    --mount type=bind,source="$(pwd)"/docker-entrypoint-initdb.d,target=/docker-entrypoint-initdb.d \
+    -v clickhouse-volume:/var/lib/clickhouse/ \
+    -v clickhouse-volume:/var/log/clickhouse-server/\
+    --mount type=bind,source="$(pwd)/docker-entrypoint-initdb.d",target=/docker-entrypoint-initdb.d \
     --ulimit nofile=262144:262144 \
     clickhouse/clickhouse-server
