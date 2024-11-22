c5d3b472632fcbf16e111f83ea4fa368fd0b5ff2
Duy Tran Nhat <nhatduy240203@gmail.com>
2024-11-22 22:08:46 +0700

    chore: update scripts

--- transform_brew.sh
+++ transform_brew.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Define variables
 CSV_FILE="brew.csv"
 SPLIT_PREFIX="brew_part_"
 SPLIT_LINES=50000
@@ -11,27 +10,21 @@ MODEL_FILE="models/combined_brew.sql"
 
 cd "$DBT_PROJECT_DIR"
 
-
-
-# Step 1: Split the CSV file into smaller files
 echo "Splitting $CSV_FILE into smaller files..."
 split -l $SPLIT_LINES $CSV_FILE $SPLIT_PREFIX
 
-# Step 2: Move split files to the seed directory and rename them
 echo "Moving split files to $SEED_DIR..."
 # mkdir -p $SEED_DIR
 for file in ${SPLIT_PREFIX}*; do
     mv "$file" "$SEED_DIR/$file.csv"
 done
 
-# Step 3: Create the seeds.yml configuration file
 for file in ${SEED_DIR}/${SPLIT_PREFIX}*.csv; do
     seed_name=$(basename "$file" .csv)
     echo "    $seed_name:" >> $SEED_CONFIG_FILE
     echo "      description: \"Part of the brew data\"" >> $SEED_CONFIG_FILE
 done
 
-# Step 4: Create the combined_brew.sql model file
 echo "Creating $MODEL_FILE..."
 echo "-- models/combined_brew.sql" > $MODEL_FILE
 echo "WITH" >> $MODEL_FILE
@@ -60,13 +53,6 @@ for file in ${SEED_DIR}/${SPLIT_PREFIX}*.csv; do
     echo "SELECT * FROM $seed_name" >> $MODEL_FILE
 done
 
-# Step 5: Load the seed data into ClickHouse
-# echo "Loading seed data into ClickHouse..."
-# cd $DBT_PROJECT_DIR
-# dbt seed
+dbt seed
 
-# Step 6: Run the dbt model to combine the data
-# echo "Running dbt model to combine the data..."
-# dbt run --models combined_brew
-
-# echo "Process completed."
+dbt run --models combined_brew
