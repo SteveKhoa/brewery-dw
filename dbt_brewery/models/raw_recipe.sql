50d3ac2a88c3274ee137644f83cb06a9379b82c3
Khoa LTN <vietnamkhoale@gmail.com>
2024-11-23 16:03:36 +0700

    hehe

--- dbt_brewery/models/raw_recipe.sql
+++ dbt_brewery/models/raw_recipe.sql
@@ -1,3 +1,5 @@
+-- THIS IS AN EXTREMELY BAD PRACTICE TO USE DBT TO INGEST DATA.
+-- ONLY FOR THE FUN OF THIS PROJECT.
 SELECT * FROM
 url('https://docs.google.com/spreadsheets/d/1SqiWJW_57nm3HKEtvxYvF6Chb6pfalFh4Doo3UU1lcs/export?gid=219143368&format=csv')
 
