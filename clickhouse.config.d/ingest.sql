4f91fd87dccde2cd8b1d5c6cd9205c3eaf835a73
Khoa LTN <vietnamkhoale@gmail.com>
2024-11-23 17:00:14 +0700

    ingest.sql hehehehehhehehahahaha

--- clickhouse.config.d/ingest.sql
+++ clickhouse.config.d/ingest.sql
@@ -1,5 +1,25 @@
 SET default_table_engine = 'Log';
 
-CREATE TABLE raw_combined_brew AS
+CREATE TABLE IF NOT EXISTS raw_combined_brew AS
 SELECT *
-FROM file('brew.csv')
+FROM file('brew.csv');
+
+CREATE TABLE IF NOT EXISTS raw_productvendor AS
+select * 
+from file('productvendor.csv');
+
+create table IF NOT EXISTS raw_purchaseorderdetail AS
+select * 
+from file('purchaseorderdetail.csv');
+
+create table IF NOT EXISTS raw_purchaseorderheader AS
+select *
+from file('purchaseorderheader.csv');
+
+create table IF NOT EXISTS raw_shipmethod AS
+select *
+from file('shipmethod.csv');
+
+create table IF NOT EXISTS raw_vendor AS
+select *
+from file('vendor.csv');
